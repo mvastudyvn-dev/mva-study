941fdaa2e2a515fbce4386c68828e810723367fb
MVA Admin <admin@mva-study.com>
2026-07-20 02:35:47 +0700

    update: configure actual course thumbnails

--- src/core/constants/courseThumbnails.ts
+++ src/core/constants/courseThumbnails.ts
@@ -1,12 +1,14 @@
 export const courseThumbnails: Record<string, string> = {
   // Thay thế các URL trống bên dưới bằng đường dẫn ảnh thật của bạn.
   // Ví dụ: 'c1': '/images/courses/c1-thumbnail.jpg'
-  'c1': '',
-  'c2': '',
-  'c3': '',
-  'c4': '',
-  'c5': '',
-  'c6': '',
-  'c7': '',
-  'c8': '',
+  'c1': '/images/courses/2k9xps.png',
+  'c2': '/images/courses/mosword.png',
+  'c3': '/images/courses/mosexcel.png',
+  'c4': '/images/courses/mosppt.png',
+  'c5': '/images/courses/ic3lv1.png',
+  'c6': '/images/courses/ic3lv2.png',
+  'c7': '/images/courses/ic3lv3.png',
+  'c8': '/images/courses/combomosic3.png',
+  'c1784486181404': '/images/courses/bttheochude.png',
+  'c1784363562926': '/images/courses/20dephattrien.png',
 };
